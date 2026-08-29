76dbdcfeca960b49ce06ab9ecb51a3a2d8c0b6d8
Youness CHAOUI <younesschaoui15@gmail.com>
2026-08-29 18:26:30 +0200

    Init commit

---
+++ src/main/java/com/chaoui/rooms/RoomsSpringMvcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.chaoui.rooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsSpringMvcSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomsSpringMvcSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chaoui/rooms/RoomsSpringMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaoui.rooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsSpringMvcSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
